334b1bc83495a0a43e65684407ffc12dce7d72a2
Zachary Lam <zlam@rrc.ca>
2026-01-03 20:43:46 -0600

    Added a Picker to choose numberOfPeople with a pickerStyle of navigationLink to move to a new View

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,14 +15,23 @@ struct ContentView: View {
     let tipPercentages: Array<Int> = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
-            
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
         }
     }
